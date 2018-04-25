c0538d47e692b392010e4f6e1b2e1489fd6edaa1
hmkrivoj <krivoj@hm.edu>
2018-04-25 20:34:18 +0200

    Creating a skeleton for settingsfragment

---
+++ lib/fragments/settingsFragment.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SettingsFragment extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+      child: new Image.asset('assets/placeholder.png'),
+    );
+  }
+
+}
